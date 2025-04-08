6eaf0957999022408c34621233900e90859f0d3f
apachish <apachish@gmail.com>
2025-04-08 10:23:23 +0330

    card

---
+++ app/Console/Commands/CreateCardDobrnaGame.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Apachish\Dabelna\App\Models\Game;
+use App\Services\DobrnaCardGenerator;
+use App\Services\DobrnaGame;
+use Illuminate\Console\Command;
+
+class CreateCardDobrnaGame extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:create-card-dobrna-game';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $array = [
+          Game::TYPE_TEST => 3,
+          Game::TYPE_RIAL => 48,
+          Game::TYPE_USDT => 48,
+        ];
+        foreach ($array as $key => $value) {
+
+
+           $game =  Game::create([
+                "lottery_date"=>now()->addHour(1),
+                "type"=>$key,
+                "num_player"=>$value
+            ]);
+            $dobrna = new DobrnaGame($value);
+            $path = "app/public/games/".Game::$types[data_get($game,"type")]."/".data_get($game,"id")."/";
+            makeDirectoryStorage($path);
+            $dobrna->generatePDF($path,data_get($game,"id"),$key);
+        }
+    }
+}
