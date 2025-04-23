a3be678501a66fc3bea79d2dd8b58190c0f8d09a
apachish <apachish@gmail.com>
2025-04-23 12:18:50 +0330

    up

--- app/Console/Commands/Test.php
+++ app/Console/Commands/Test.php
@@ -29,21 +29,21 @@ class Test extends Command
      */
     public function handle()
     {
-//        $game_server = new GameService();
-//        $game = Game::with("winners")->where("id", 4)->first();
-//        $game_server->sendWinner($game);
-        if (str_contains($this->message_cache, "charging_usdt_getFile"))
-            echo "charging_usdt_getFile";
-        elseif (str_contains($this->message_cache, "charging_usdt"))
-            echo "charging_usdt";
+        $game_server = new GameService();
+        $game = Game::with("winners")->where("id", 5)->first();
+        $game_server->sendWinner($game);
+//        if (str_contains($this->message_cache, "charging_usdt_getFile"))
+//            echo "charging_usdt_getFile";
+//        elseif (str_contains($this->message_cache, "charging_usdt"))
+//            echo "charging_usdt";
 
 
 
-        elseif (str_contains($this->message_cache, "withdrawal_usdt_"))
-            echo "withdrawal_usdt_";
-
-        elseif (str_contains($this->message_cache, "get_withdrawal_usdt_"))
-            echo "get_withdrawal_usdt_";
+//        elseif (str_contains($this->message_cache, "withdrawal_usdt_"))
+//            echo "withdrawal_usdt_";
+//
+//        elseif (str_contains($this->message_cache, "get_withdrawal_usdt_"))
+//            echo "get_withdrawal_usdt_";
 
     }
 }
